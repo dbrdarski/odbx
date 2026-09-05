9303a37460ed922e6aa597c309bd8031c5b39fe5
dbrdarski <dane.brdarski@gmail.com>
2026-09-06 00:52:45 +0200

    Use strings for persistent references

--- src/symbols.mjs
+++ src/symbols.mjs
@@ -1,16 +1,10 @@
 import { encodeInt } from './codec.mjs';
 
-const reference = type => id => {
-  const token = `${type}${encodeInt(id)}`;
-  return Object.freeze({ type, id, toString: () => token });
-};
+const reference = type => id => `${type}${encodeInt(id)}`;
 
 export const stringReference = reference('S');
 export const tupleReference = reference('A');
 export const recordReference = reference('O');
 export const revisionReference = reference('R');
 
-export const documentReference = typeId => id => {
-  const token = `D${encodeInt(typeId)}:${encodeInt(id)}`;
-  return Object.freeze({ type: 'D', typeId, id, toString: () => token });
-};
+export const documentReference = typeId => id => `D${encodeInt(typeId)}:${encodeInt(id)}`;
